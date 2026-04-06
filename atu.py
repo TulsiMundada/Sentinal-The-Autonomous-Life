77f93e51904bd5d45f8e16cab660c2a20e1df321
apoorvarajendra24 <apoorva.patrey22@gmail.com>
2026-04-06 11:17:39 +0000

    deleted unnecessary files from the repo

--- atu.py
+++
@@ -1,6 +0,0 @@
-Hi Apoorva Barapatre!
-You make me happy and I love you more than u can imagine!
-but sometimes u make my brain boil like literally i hate u to core and still love u more than anything else..
-you are my everthing and i do want to get married with you 
-so thanks for being in my life
-and thank u so much for handling me!!
